d16ee9b55cd7fe6d6297a9d82db97efbdda86103
Amina Shellman <aminalshellman3@gmail.com>
2022-10-16 07:54:37 -0700

    Initial Commit

---
+++ Apper/ApperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApperApp.swift
+//  Apper
+//
+//  Created by Amina Shellman on 10/16/22.
+//
+
+import SwiftUI
+
+@main
+struct ApperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
